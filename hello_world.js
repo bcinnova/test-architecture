8f058e85bfb6b87b00a0f72a6e4b7ed87621f9f3
Amir Mehmood <amehmood@cinnova.com>
2014-09-15 16:30:55 +0500

    Changes has been made from mac

--- hello_world.js
+++ hello_world.js
@@ -1,8 +1,9 @@
 var http = require("http");
 http.createServer(function(request, response) {  
   response.writeHead(200, {"Content-Type": "text/plain"});  
-  response.write("Hello from the Node.js server!");  
+  response.write("Hello from the Node.js server!");
   response.write("Hello from local PC! update 2");
+  response.write("Hello from local PC! update MAC");
   response.end();
 }).listen(8080);
 console.log('Server is listening to http://localhost/ on port 8080…');
